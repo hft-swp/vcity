92be2444d2c777781f5ae64ab50dc94a75117402
02grst1bif <02grst1bif@hft-stuttgart.de>
2014-04-07 10:50:05 +0200

    Edit features for Parser

--- vCity/src/de/hft_stuttgart/swp2/parser/MyHouseTest.java
+++ vCity/src/de/hft_stuttgart/swp2/parser/MyHouseTest.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Vector;
 
 import org.citygml4j.CityGMLContext;
