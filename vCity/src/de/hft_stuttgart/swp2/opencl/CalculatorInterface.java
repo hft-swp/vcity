fbfafb89bc7ccbd2f0e1302002e88feda3292fe6
12riju1bif <12riju1bif@IL-2-260-105>
2014-04-28 11:39:58 +0200

    added javadoc for interface + shadowcalc in openCL fix but slow JOCLDeviceQuery for device infos

--- vCity/src/de/hft_stuttgart/swp2/opencl/CalculatorInterface.java
+++ vCity/src/de/hft_stuttgart/swp2/opencl/CalculatorInterface.java
@@ -1,8 +1,29 @@
 package de.hft_stuttgart.swp2.opencl;
 
 public interface CalculatorInterface {
-	
+
+	/**
+	 * This method calculates the volume of all buildings stored in the city
+	 * singleton.
+	 * 
+	 * @throws OpenClException
+	 *             if the initialization of the OpenCL context creation fails.
+	 */
 	public void calculateVolume() throws OpenClException;
-	public void calculateShadow(ShadowPrecision precision) throws OpenClException;
+
+	/**
+	 * This method calculates the shadows of all buildings stored in the city
+	 * singleton. The triangles are therefore split into smaller triangles
+	 * called ShadowTriangles to ensure a certain amount of precision for the
+	 * calculation. The maximum area can be set by the precision parameter. The
+	 * calculation itself is not a 100% accurate.
+	 * 
+	 * @param precision
+	 *            level of precision.
+	 * @throws OpenClException
+	 *             if the initialization of the OpenCL context creation fails.
+	 */
+	public void calculateShadow(ShadowPrecision precision)
+			throws OpenClException;
 
 }
--- vCity/src/de/hft_stuttgart/swp2/opencl/ShadowPrecision.java
+++ vCity/src/de/hft_stuttgart/swp2/opencl/ShadowPrecision.java
@@ -1,8 +1,20 @@
 package de.hft_stuttgart.swp2.opencl;
 
 public enum ShadowPrecision {
-	
-	VERY_LOW(5), LOW(2.5f), MID(1.25f), HIGH(0.75f), ULTRA(0.375f), HYPER(0.1f), AWESOME(0.01f);
+	/** Area maximum is 5 */
+	VERY_LOW(5),
+	/** Area maximum is 2.5 */
+	LOW(2.5f),
+	/** Area maximum is 1.25 */
+	MID(1.25f),
+	/** Area maximum is 0.75 */
+	HIGH(0.75f),
+	/** Area maximum is 0.375 */
+	ULTRA(0.375f),
+	/** Area maximum is 0.1 */
+	HYPER(0.1f),
+	/** Area maximum is 0.01 */
+	AWESOME(0.01f);
 	
 	
 	private float area;
