015cc1a35d6046bf02ed2ce70e6c6a396de3b286
juhulian <julian@rilli.eu>
2014-05-02 12:07:20 +0200

    remove old comments

--- vCity/src/de/hft_stuttgart/swp2/render/city3d/CityMap3D.java
+++ vCity/src/de/hft_stuttgart/swp2/render/city3d/CityMap3D.java
@@ -77,8 +77,6 @@ public class CityMap3D extends JFrame implements GLEventListener{
 		City.getInstance().addBuilding(b);
 	
 		CalculatorInterface backend = new CalculatorImpl();
-//		ShadowCalculatorInterface backend = new ShadowCalculatorOpenClBackend();
-//		ShadowCalculatorInterface backend = new ShadowCalculatorJavaBackend();
 		System.out.println("Starting shadow calculation...");
 		long start = System.currentTimeMillis(); 
 		backend.calculateShadow(ShadowPrecision.ULTRA); //VERY_LOW(5), LOW(2.5f), MID(1.25f), HIGH(0.75f), ULTRA(0.375f), HYPER(0.1f), AWESOME(0.01f)
