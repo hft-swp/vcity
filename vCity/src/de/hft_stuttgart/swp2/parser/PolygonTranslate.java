8e73742b0a229fa80ea9c09093a112a35e3a2bdc
juhulian <julian@rilli.eu>
2014-05-07 14:04:58 +0200

    koordinaten transformation

--- vCity/src/de/hft_stuttgart/swp2/parser/PolygonTranslate.java
+++ vCity/src/de/hft_stuttgart/swp2/parser/PolygonTranslate.java
@@ -16,8 +16,10 @@ public class PolygonTranslate {
 	 * @author 02sial1bif, 02grst1bif
 	 */
 	public static ArrayList<Vertex> translateToOrigin(ArrayList<Vertex> poly,double[] reference) {
-
+ 
+		double rotateBy;
 		Matrix translate;
+		Matrix rotate;
 		ArrayList<Vertex> polynew = new ArrayList<>();
 
 		for (int i = 0; i < poly.size(); i++) {
@@ -27,16 +29,30 @@ public class PolygonTranslate {
 			double z = poly.get(i).getZ();
 
 			//TODO Koordinatenachsen transformieren
-			//EPSG auslesen
+			//EPSG auslesen		
+			
 			
 			translate = new Matrix(new double[][] {
 					{ 1, 0, 0, (reference[0]) * -1 },
 					{ 0, 1, 0, (reference[1]) * -1 },
-					{ 0, 0, 1, (reference[2]) * -1 }, { 0, 0, 0, 1 } });
+					{ 0, 0, 1, (reference[2]) * -1 }, 
+					{ 0, 0, 0, 1 } });
+			
+			rotateBy = -Math.toRadians(90);
+			rotate = new Matrix(new double[][]{
+					{1,0,0,0},
+					{0,Math.cos(rotateBy),-Math.sin(rotateBy),0},
+					{0,Math.sin(rotateBy),Math.cos(rotateBy),0},					
+					{0,0,0,1}});
+			
+			
+			
+			
 
 			double[] vals = new double[] { x, y, z, 1 };
 			Matrix v = new Matrix(vals, 1);
-			Matrix trans = translate;
+			
+			Matrix trans = rotate.times(translate);
 			double[] newVals = trans.times(v.transpose()).toColumnPackedArray();
 
 			polynew.add(new Vertex((float) newVals[0], (float) newVals[1], (float) newVals[2]));
