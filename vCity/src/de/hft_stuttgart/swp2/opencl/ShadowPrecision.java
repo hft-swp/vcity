4028c4766312332736b9debcb87de25c5897c63c
x3no222 <x3no89@gmx.de>
2014-05-07 09:42:26 +0200

    Added readable Name with getName() for ShadowPrecision

--- vCity/src/de/hft_stuttgart/swp2/opencl/ShadowPrecision.java
+++ vCity/src/de/hft_stuttgart/swp2/opencl/ShadowPrecision.java
@@ -2,29 +2,34 @@ package de.hft_stuttgart.swp2.opencl;
 
 public enum ShadowPrecision {
 	/** Area maximum is 5 */
-	VERY_LOW(5),
+	VERY_LOW(5, "Very low"),
 	/** Area maximum is 2.5 */
-	LOW(2.5f),
+	LOW(2.5f, "Low"),
 	/** Area maximum is 1.25 */
-	MID(1.25f),
+	MID(1.25f, "Mid"),
 	/** Area maximum is 0.75 */
-	HIGH(0.75f),
+	HIGH(0.75f, "High"),
 	/** Area maximum is 0.375 */
-	ULTRA(0.375f),
+	ULTRA(0.375f, "Ultra"),
 	/** Area maximum is 0.1 */
-	HYPER(0.1f),
+	HYPER(0.1f, "Hyper"),
 	/** Area maximum is 0.01 */
-	AWESOME(0.01f);
+	AWESOME(0.01f, "Awesome");
 	
 	
 	private float area;
+	private String name;
 	
-	private ShadowPrecision(float area) {
+	private ShadowPrecision(float area, String name) {
 		this.area = area;
 	}
 	
 	public float getArea() {
 		return area;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 }
