06dc709dec9632bd9199f0549393ef19e8c24c1b
02grst1bif <02grst1bif@gdv112.fkc.hft-stuttgart.de>
2014-05-07 09:00:55 +0200

    Updated Parser Test

--- vCity/src/de/hft_stuttgart/swp2/parser/test/ParserTest.java
+++ vCity/src/de/hft_stuttgart/swp2/parser/test/ParserTest.java
@@ -8,7 +8,9 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import de.hft_stuttgart.swp2.model.Building;
 import de.hft_stuttgart.swp2.model.City;
+import de.hft_stuttgart.swp2.model.Triangle;
 import de.hft_stuttgart.swp2.model.Vertex;
 import de.hft_stuttgart.swp2.parser.CGMLParser;
 import de.hft_stuttgart.swp2.parser.PolygonTranslate;
@@ -22,17 +24,53 @@ public class ParserTest {
 
 	}
 
-	@Ignore
 	@Test
 	public void testParseCity() {
-		City c = null;
-		
 		try {
-			c = CGMLParser.getInstance().parse(fileName);
+
+			CGMLParser parser = CGMLParser.getInstance();
+			assertNotNull(parser);
+
+			City city = parser.parse(fileName);
+			assertNotNull(city);
+		
+			ArrayList<Building> bs = city.getBuildings();
+			assertNotNull(bs);
+			assertTrue(bs.size() == 1);
+			
+			Building b = bs.get(0);
+			assertNotNull(b);
+//			String bName = b.getId();
+			
+			ArrayList<Triangle> tri = b.getTriangles();
+			assertNotNull(tri);
+			assertTrue(tri.size() == 12);
+
+			Triangle t = tri.get(0);
+			assertNotNull(t);
+
+			Vertex[] v = t.getVertices();
+			assertNotNull(v);
+			
+			int len = v.length;
+			assertTrue(len == 3);
+			
+			assertEquals(v[0].getX(), 8.09f, 0.01f);
+			assertEquals(v[0].getY(), -0.67f, 0.01f);
+			assertEquals(v[0].getZ(), 0.0f, 0.01f);
+			
+			assertEquals(v[1].getX(), 0.09f, 0.01f);
+			assertEquals(v[1].getY(), -0.17f, 0.01f);
+			assertEquals(v[1].getZ(), 0.0f, 0.01f);
+			
+			assertEquals(v[2].getX(), 0.34f, 0.01f);
+			assertEquals(v[2].getY(), 5.33f, 0.01f);
+			assertEquals(v[2].getZ(), 0.0f, 0.01f);
+			
 		} catch (Exception e) {
+			e.printStackTrace();
 			fail();
 		}
-		assertNotNull(c);
 	}
 	
 	@Test
@@ -52,9 +90,5 @@ public class ParserTest {
 		
 	}
 
-	@Ignore
-	@Test
-	public void testTriangulate() {
-		
-	}
+	
 }
