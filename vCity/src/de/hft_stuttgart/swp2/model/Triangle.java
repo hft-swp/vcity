6a2ca103f3aaa53e6fc84a5baf29c10e6aee2ec9
12riju1bif <12riju1bif@IL-2-260-105>
2014-05-19 11:36:20 +0200

    add normal vector attribute

--- vCity/src/de/hft_stuttgart/swp2/model/Triangle.java
+++ vCity/src/de/hft_stuttgart/swp2/model/Triangle.java
@@ -9,9 +9,24 @@ package de.hft_stuttgart.swp2.model;
 public class Triangle {
 
 	private final Vertex[] vertices;
+	private Vertex normalVector;
 
 	/**
-	 * creates a new triangle out of 3 vertcies. If anything other than 3
+	 * @return the normalVector
+	 */
+	public Vertex getNormalVector() {
+		return normalVector;
+	}
+
+	/**
+	 * @param normalVector the normalVector to set
+	 */
+	public void setNormalVector(Vertex normalVector) {
+		this.normalVector = normalVector;
+	}
+
+	/**
+	 * creates a new triangle out of 3 vertices. If anything other than 3
 	 * vertices is given this throws a IllegalArgumentException.
 	 * 
 	 * @param vertices
