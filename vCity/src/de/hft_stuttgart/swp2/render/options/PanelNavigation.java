3fa98b9823860cb83c0e1d1236ffb3b490cef1bf
Marcel Ruckaberle <mrc1009@gmx.de>
2014-06-09 21:45:17 +0200

    Added a slider to zoom without the mouse wheel

--- vCity/src/de/hft_stuttgart/swp2/render/options/PanelNavigation.java
+++ vCity/src/de/hft_stuttgart/swp2/render/options/PanelNavigation.java
@@ -5,14 +5,19 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import de.hft_stuttgart.swp2.render.Main;
 
-public class PanelNavigation extends JPanel implements ItemListener{
+public class PanelNavigation extends JPanel implements ItemListener, ChangeListener{
 
 	/**
 	 * 
@@ -22,7 +27,7 @@ public class PanelNavigation extends JPanel implements ItemListener{
 	private JButton btnDown = new JButton("Down");
 	private JButton btnLeft = new JButton("Left");
 	private JButton btnRight = new JButton("Right");
-	
+	private JSlider jSliderZoom = new JSlider(JSlider.HORIZONTAL, 20, 100, 60);
 	
 	public PanelNavigation() {
 		this.setLayout(new GridLayout(3, 3));
@@ -30,20 +35,33 @@ public class PanelNavigation extends JPanel implements ItemListener{
 		this.add(btnUp);
 		btnUp.addItemListener(this);
 		this.add(new JLabel());
-		
 		this.add(btnLeft);
-		this.add(new JLabel());
+		// Die Abstände zwischen den 
+		// Teilmarkierungen werden festgelegt
+		jSliderZoom.addChangeListener(this);
+		jSliderZoom.setMajorTickSpacing(40);
+		jSliderZoom.setMinorTickSpacing(20);
+	    // create a new hashtable
+	    Dictionary <Integer, JLabel> dict = new Hashtable <Integer, JLabel>();
+	    // add elements in the hashtable
+	    dict.put(20, new JLabel("20"));
+	    dict.put(60, new JLabel("50"));
+	    dict.put(100, new JLabel("100"));
+		jSliderZoom.setLabelTable(dict);
+		// Standardmarkierungen werden erzeugt 
+		//jSliderZoom.createStandardLabels(1);
+		// Zeichnen der Markierungen wird aktiviert
+		jSliderZoom.setPaintTicks(true);
+		// Zeichnen der Labels wird aktiviert
+		jSliderZoom.setPaintLabels(true);
+		this.add(jSliderZoom);
 		this.add(btnRight);
-		
 		this.add(new JLabel());
 		this.add(btnDown);
 		this.add(new JLabel());
-		
 		addActionListeners();
-		
 	}
-	
-	
+		
 	private void addActionListeners(){
 		btnUp.addActionListener(new ActionListener() {
 			@Override
@@ -95,6 +113,24 @@ public class PanelNavigation extends JPanel implements ItemListener{
 		if(source == btnUp){
 			System.out.println(ItemEvent.SELECTED);
 			setCameraUp();
+		}
+	}
+
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		Object source = e.getSource();
+		if(source == jSliderZoom){
+			int currentCameraValue = (int)Main.getCityMap3D().camera.positionY ;
+			int currentSliderValue = jSliderZoom.getValue();
+			int diff;
+			if(currentSliderValue < currentCameraValue){
+				diff = currentCameraValue - currentSliderValue;
+				Main.getCityMap3D().camera.moveForward(diff);
+			}else if(currentCameraValue < currentSliderValue){
+				diff = currentSliderValue - currentCameraValue;
+				Main.getCityMap3D().camera.moveBackwards(diff);
+			}
 		}
 	}
 
