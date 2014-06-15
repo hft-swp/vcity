8a83b9d3548997b436d3f44b91265afcce6a38f5
Sinan Altintop <12alsi1bif@hft-stuttgart.de>
2014-06-15 16:10:13 +0200

    deleted unused method
    Method not needed

--- vCity/src/de/hft_stuttgart/swp2/parser/PolygonTranslate.java
+++ vCity/src/de/hft_stuttgart/swp2/parser/PolygonTranslate.java
@@ -55,43 +55,4 @@ public class PolygonTranslate {
 		return polynew;
 	}
 
-	
-	
-	/**
-	 * Translates a vertex back to his initial value
-	 * @param vert Vertex to translate
-	 * @param reference Vertex with smallest x coordinate has to be the same as {@link #translateToOrigin(ArrayList, double[])} 
-	 * @return VertexDouble with initial value
-	 */
-	public static VertexDouble translateBack(Vertex vert, double[] reference) {
-
-		double rotateBy;
-		Matrix translate;
-		Matrix rotate;
-
-		double x = vert.getX();
-		double y = vert.getY();
-		double z = vert.getZ();
-
-		translate = new Matrix(new double[][] {
-				{ 1, 0, 0, (reference[0]) },
-				{ 0, 1, 0, (reference[1]) },
-				{ 0, 0, 1, (reference[2]) },
-				{ 0, 0, 0, 1 } });
-
-		rotateBy = Math.toRadians(90);
-		rotate = new Matrix(new double[][] {
-				{ 1, 0, 0, 0 },
-				{ 0, Math.cos(rotateBy),-Math.sin(rotateBy), 0 },
-				{ 0, Math.sin(rotateBy), Math.cos(rotateBy), 0 },
-				{ 0, 0, 0, 1 } });
-
-		double[] vals = new double[] { x, y, z, 1 };
-		Matrix v = new Matrix(vals, 1);
-
-		Matrix trans = translate.times(rotate);
-		double[] newVals = trans.times(v.transpose()).toColumnPackedArray();
-
-		return new VertexDouble(newVals[0], newVals[1], newVals[2]);
-	}
 }
